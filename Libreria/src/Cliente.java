96d16700d1ebd01939905a38d553c4388b56fbbd
Androide 23 <juanestebannino970830@gmail.com>
2026-09-10 21:16:30 -0500

    Se crea la clase Cliente, constructor GandS

---
+++ Libreria/src/Cliente.java
@@ -0,0 +1,39 @@
+public class Cliente {
+	private String id;
+	private String nombre;
+	private String telefono;
+	private String direccion;
+	private boolean tieneLibroPrestado;
+
+	public Cliente(String id, String nombre, String telefono, String direccion) {
+		this.id = id;
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.direccion = direccion;
+		this.tieneLibroPrestado = false;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public boolean tieneLibroPrestado() {
+		return tieneLibroPrestado;
+	}
+
+	public void setTieneLibroPrestado(boolean tieneLibroPrestado) {
+		this.tieneLibroPrestado = tieneLibroPrestado;
+	}
+}
