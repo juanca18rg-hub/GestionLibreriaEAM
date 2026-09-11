5257cf070771fbd7b3497d891a29da06d89efbbb
Camilo <juanca18rg@gmail.com>
2026-09-10 21:29:14 -0500

    Se anadieron atributos a libreria

--- Libreria/src/Libreria.java
+++ Libreria/src/Libreria.java
@@ -1,5 +1,10 @@
 import java.util.ArrayList;
 
 public class Libreria {
+    ArrayList<Cliente> clientes;
+    ArrayList<Libro> libros;
+    ArrayList<Prestamo> prestamos;
+    ArrayList<Devolucion> devoluciones;
+    
 }
 
